9c5748e1642c30321d9fed1a0f40943958dfe20e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-12-14 15:12:25 +0530

    server is up

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("Welcome to my gym backend project")
+const express=require("express")
+const cors=require("cors")
+const app=express()
+const db=require("./db")
+const bodyParser=require("body-parser")
+require("dotenv").config()
+
+//for now enabling all requests
+app.use(cors())
+
+//here it will convert the json string/data to js object and save it inside: req.body
+app.use(bodyParser.json())
+
+const PORT=process.env.PORT || 3000
+
+//importing router files
+
+//using routes
+
+//just for testing: http://localhost:3000/
+app.get("/",(req,res)=>{
+    res.send("Welcome to gym app backend...")
+})
+
+app.listen(PORT,()=>{
+    console.log("server is running on: http://localhost:3000/")
+})
