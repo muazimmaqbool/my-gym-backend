5bd5b0718cd60406e98f4542d730dc291f6012bc
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-12-16 22:20:48 +0530

    jwt token returned on login

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 //getting user model schema
 const User = require("../Models/user");
+const { generateToken } = require("../jwt");
 
 //saving data of user
 //use likt this: http://localhost:3000/user/signup or url/user/signup
@@ -23,24 +24,31 @@ router.post("/signup", async (req, res) => {
 });
 
 //login route : /user/login
-router.post("/login",async(req,res)=>{
-  try{
+router.post("/login", async (req, res) => {
+  try {
     //getting email and password of the user
-    const{email,password}=req.body;
+    const { email, password } = req.body;
     //getting the user by email
-    const user=await User.findOne({email:email})
+    const user = await User.findOne({ email: email });
     //if user is not found or either email or password is wrong
     //? write checking/compare password logic later
-    if(!user){
-      return res.status(401).json("Invalid email or password")
+    if (!user) {
+      return res.status(401).json("Invalid email or password");
     }
+    // console.log("user found:", user);
+    //payload for jwt token
+    const jwtPayload = {
+      id: user?._id,
+      name: user?.name,
+    };
+    const token = generateToken(jwtPayload);
     //is user is found
-    res.status(200).json({message:"User login successfully!"})
-  }catch (error) {
+    res.status(200).json({ message: "User login successfully!", token: token });
+  } catch (error) {
     console.log("Error while saving user data:", error);
     res.status(500).json({ error: "Internal server error" });
   }
-})
+});
 
 //getting user profile
 
