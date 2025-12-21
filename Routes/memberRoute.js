ba9b09ac67fcd7074eedffff468a885032f2ec2d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-12-21 11:56:57 +0530

    Getting all members route added

--- Routes/memberRoute.js
+++ Routes/memberRoute.js
@@ -16,4 +16,23 @@ router.post("/register", jwtAuthMiddleware, async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+
+//getting all members
+router.get("/all",jwtAuthMiddleware,async(req,res)=>{
+    try{
+        const allMembers=await Member.find()
+        // console.log("allMembers:",allMembers)
+        if(allMembers.length===0){
+            res.status(200).json({message:"No record found"})
+        }
+        res.status(200).json(allMembers)
+    }catch (err) {
+    console.log("Error while registering new member:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+})
+
+
+
 module.exports = router;
